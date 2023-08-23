b932b4571d698d66378706f7c9823ebd7482f534
d-v-d-k <36768779+d-v-d-k@users.noreply.github.com>
2023-08-23 06:52:14 +0200

    Update and rename Connect-WiFi.ps1 to General/Connect-WiFi.ps1

--- General/Connect-WiFi.ps1
+++ General/Connect-WiFi.ps1
@@ -6,6 +6,5 @@ netsh wlan connect name=ExampleWiFi | Out-Null
 
 Write-Host "`rConnected to Wi-Fi." -NoNewLine -ForegroundColor Green
 
-# in plaats van wachten, test elke 3s de connectie (Test-NetConnection/get-netadapter (wanneer laatste - is up ook up als hij wacht op confirm?)
 Write-Host "`rPress [Enter] when you finished the confirmation page." -NoNewLine -ForegroundColor Yellow; Read-Host
 Write-Host "`r" -ForegroundColor Green
