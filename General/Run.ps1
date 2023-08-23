147bab19bce1b2d0d3085dfa7ef05a66032d6989
d-v-d-k <36768779+d-v-d-k@users.noreply.github.com>
2023-08-23 06:53:51 +0200

    Add files via upload

---
+++ General/Run.ps1
@@ -0,0 +1,9 @@
+Run-General "Rename-Computer"
+Run-General "Install-Chrome"
+Run-General "Install-AdobePDF"
+
+Run-Specific "Uninstall-Bloatware"
+Run-Specific "Join-WiFi"
+Run-Specific "Join-Domain"
+
+Run-General "Ask-Restart"
