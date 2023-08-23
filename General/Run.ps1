60dc5538379adab71a547c22f2dc32f041ecdce5
d-v-d-k <36768779+d-v-d-k@users.noreply.github.com>
2023-08-23 06:54:24 +0200

    Delete Run.ps1

--- General/Run.ps1
+++
@@ -1,9 +0,0 @@
-Run-General "Rename-Computer"
-Run-General "Install-Chrome"
-Run-General "Install-AdobePDF"
-
-Run-Specific "Uninstall-Bloatware"
-Run-Specific "Join-WiFi"
-Run-Specific "Join-Domain"
-
-Run-General "Ask-Restart"
